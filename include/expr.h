169d13511fd4947ce63dee974ec2055208ac3fc1
Matthew <127076667+hufaloofa@users.noreply.github.com>
2025-07-24 17:10:32 +1000

    added expr for general

--- include/expr.h
+++ include/expr.h
@@ -1,18 +1,26 @@
+#ifndef EXPR_H
+#define EXPR_H
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "lexer.h"
 
+typedef struct Expr Expr;
+
+
 typedef enum {
-    EXPR_NONE,
-    EXPR_BOOL,
-    EXPR_NUMBER,
-    EXPR_STRING,
+    EXPR_NONE, //??
+    EXPR_NIL, 
+    EXPR_BOOL, 
+    EXPR_NUMBER,  
+    EXPR_STRING, 
     EXPR_UNARY,
     EXPR_BINARY,
     EXPR_GROUPING
 } ExprType;
 
-typedef struct {
+struct Expr{
     ExprType type;
     union {
         struct {
@@ -26,15 +34,25 @@ typedef struct {
 
         struct {
             Token* op;
-            Expr rhs;
+            Expr *rhs;
         } unary;
 
         struct {
             Token* op;
-            Expr lhs;
-            Expr rhs;
+            Expr *lhs;
+            Expr *rhs;
         } binary;
 
         Expr *grouping;
     };
-} Expr;
+};
+
+Expr *make_num_expr(double num, Token *token);
+Expr *make_string_expr(char *string, Token *token);
+Expr *make_bool_expr(bool b, Token* token);
+Expr *make_nil_expr(Token *token);
+Expr *make_unary_expr(Token *op, Expr* rhs);
+Expr *make_binary_expr(Token* op, Expr* lhs, Expr* rhs);
+Expr *make_grouping_expr(Expr *inner);
+
+#endif
--- src/expr.c
+++ src/expr.c
@@ -1,3 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "../include/expr.h"
+
+Expr *make_expr(ExprType t) {
+    Expr *expr = malloc(sizeof(Expr));
+    expr->type = t;
+    return expr;
+}
+
+Expr *make_literal_expr(ExprType et, Token *token) {
+    Expr *expr = make_expr(et);
+    expr->literal.token = token;
+    return expr;
+}
+
+Expr *make_num_expr(double num, Token *token) {
+    Expr *expr = make_literal_expr(EXPR_NUMBER, token);
+    expr->literal.number = num;
+    return expr;
+}
+
+Expr *make_string_expr(char *string, Token *token) {
+    Expr *expr = make_literal_expr(EXPR_STRING, token);
+    expr->literal.string = string;
+    return expr;
+}
+
+Expr *make_bool_expr(bool b, Token* token) {
+    Expr *expr = make_literal_expr(EXPR_BOOL, token);
+    expr->literal.boolean = b;
+    return expr;
+}
+
+Expr *make_nil_expr(Token *token) {
+    return make_literal_expr(EXPR_NIL, token);
+}
+
+Expr *make_unary_expr(Token* op, Expr* rhs) {
+    Expr *expr = make_expr(EXPR_UNARY);
+    expr->unary.op = op;
+    expr->unary.rhs = rhs;
+    return expr;
+}
+
+Expr *make_binary_expr(Token* op, Expr* lhs, Expr* rhs) {
+    Expr *expr = make_expr(EXPR_BINARY);
+    expr->binary.op = op;
+    expr->binary.lhs = lhs;
+    expr->binary.rhs = rhs;
+    return expr;
+}
+
+Expr *make_grouping_expr(Expr *inner) {
+    Expr *expr = make_expr(EXPR_GROUPING);
+    expr->grouping = inner;
+    return expr;
+}
